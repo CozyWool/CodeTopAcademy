7f29d4f0d0252e7248993ff9f0b5bf5907fa1b2c
CozyWool <vsergeev201530@gmail.com>
2024-07-01 19:06:34 +0500

    Почистил репозиторий

--- ASP.NET/CrudWebApp/CrudWebApp/Program.cs
+++
@@ -1,7 +0,0 @@
-using CrudWebApp;
-
-CreateHostBuilder(args).Build().Run();
-
-IHostBuilder CreateHostBuilder(string[] args) =>
-    Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
--- ASP.NET/ShopDemoWebApp/ShopDemoWebApp/Models/CarsModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using ShopDemoWebApp.DataAccess.Entities;
-
-namespace ShopDemoWebApp.Models;
-
-public class CarsModel
-{
-    public CarEntity[] Cars { get; set; }
-    public string CurrentCategory { get; set; }
-}
