394fbb4c26a11cbd79ca26ed4b2523ce9e01f2e5
CozyWool <vsergeev201530@gmail.com>
2024-07-05 20:52:14 +0500

    Пагинация и таг хелперы

---
+++ ASP.NET/CrudWebPaginationTagHelpers/CrudWeb/Formatters/ByteArrayInputFormatter.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+
+namespace CrudWeb.Formatters;
+
+public class ByteArrayInputFormatter : InputFormatter
+{
+    public ByteArrayInputFormatter()
+    {
+        SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse("application/octet-stream"));
+    }
+    public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
+    {
+        var stream = new MemoryStream();
+        await context.HttpContext.Request.Body.CopyToAsync(stream);
+        return await InputFormatterResult.SuccessAsync(stream.ToArray());
+    }
+}
---
+++ ASP.NET/CrudWebPaginationTagHelpers/CrudWeb/Services/IFileService.cs
@@ -0,0 +1,7 @@
+﻿namespace CrudWeb.Services;
+
+public interface IFileService
+{
+    Task<bool> UploadFile(int id, IFormFile file);
+    Task<bool> UploadFile(int id, byte[] content);
+}
