75e5ffa7f994756b442b1822bb99061c1169baf3
CozyWool <vsergeev201530@gmail.com>
2025-03-14 20:21:38 +0500

    Базовое ТЗ сделал

---
+++ Java/sportShop/src/test/java/com/exam/sportShop/SportShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.sportShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
